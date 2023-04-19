66245a2743d73b91c0f02e5f4dedb92a2d4b3d30
sjadhav1983 <131287907+sjadhav1983@users.noreply.github.com>
2023-04-19 22:37:40 +0530

    Create tds_graded_assignment8.py

---
+++ tds_graded_assignment8.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title("Find the largest among the 3 numbers")
+num1 = st.number_input("Enter number 1")
+num2 = st.number_input("Enter number 2")
+num3 = st.number_input("Enter number 3")
+calculate = st.button("Find largest number")
+def largest_number(a,b,c):
+   if a > b and a > c:
+      return a
+   elif b > a and b > c:
+      return b
+   else:
+      return c
+if calculate:
+  largest_num=largest_number(num1,num2,num3)
+  st.write("The largest number is:",largest_num)
